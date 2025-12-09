19d4a0d6b58fafc5e95e3c2abe77b33288a34f16
g4b3ll <32038696+g4b3ll@users.noreply.github.com>
2025-12-09 17:18:38 -0300

    Update footer text

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -2,7 +2,7 @@ import styles from "./Rodape.module.css";
 function Rodape() {
 	return (
 		<footer className={styles.rodape}>
-			<h2>Desenvolvido por Alura.</h2>
+			<h2>Desenvolvido por G4B3LL.</h2>
 		</footer>
 	);
 }
